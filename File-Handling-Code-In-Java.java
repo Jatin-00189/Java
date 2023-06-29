70be8549d93ca51d942e17def769d680170c59fb
0X54nj4Y_j4nGr4 <106859771+Jatin-00189@users.noreply.github.com>
2023-06-29 04:48:21 -1200

    Create File-Handling-Code-In-Java.java
    Each operation is defined between comments. You can use the code for each diffrent operation . e.g  Creating file , Reading file , Deleting file , Writing File

---
+++ File-Handling-Code-In-Java.java
@@ -0,0 +1,58 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+        File myFile = new File("file1.txt");
+
+        try{
+            myFile.createNewFile();
+        }catch(Exception e){
+            System.out.println("Exception Occured");
+            e.printStackTrace();
+        } */
+
+        //Writing a File
+
+        /* try {
+            FileWriter fileWriter = new FileWriter("file.txt");
+            fileWriter.write("Hello Guys \n Ok bye ");
+            fileWriter.close();
+            System.out.println("Writed");
+            fileWriter.
+        } catch (IOException e) {
+            System.out.println("Exception");
+            e.printStackTrace();
+        }  */
+
+        // Reading File
+
+        /*
+        try {
+            File myFile = new File("file.txt");
+            Scanner sc = new Scanner(myFile);
+
+            while (sc.hasNextLine()){
+                String data = sc.nextLine();
+                System.out.println(data);
+            }
+
+        }catch (IOException e){
+            System.out.println("Exception ocurred");
+            e.printStackTrace();
+        }
+        */
+
+        //Delete a file
+
+        /*
+        File myfile = new File("Bank.java");
+
+        if(myfile.delete()){
+            System.out.println("Hai Bhai");
+        }else {
+            System.out.println("nahi h");
+        }
+        */
+
+
+    }
+}
